e7e7c753baad96ffa47f273cd62aae7d57b7e7e8
karina-krupodior <karinakrupodior@gmail.com>
2025-06-20 10:18:56 -0500

    Spring Boot API with HomeController

---
+++ NorthwindTradersAPI/src/main/java/controller/HomeController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@RequestParam(defaultValue = "World") String country) {
+        return "Hello " + country + "!";
+    }
+}
